901cf5eacbd0cef7d790e0b48c1189850cad10cb
Piotr <piotrwanio@gmail.com>
2018-09-19 17:35:35 +0200

    changed photo on main page

--- assets/components/ContentComponent.vue
+++ assets/components/ContentComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="content">
          <div id="section-a">
-            <div class="background-image" :style="{ backgroundImage: 'url(' + require('../assets/Fotolia_51053_M.jpg') + ')' }">
+            <div class="background-image" :style="{ backgroundImage: 'url(' + require('../assets/baby-baby-feet-barefoot-1172922.jpg') + ')' }">
             </div>
             <h1> Centrum rehabilitacji i integracji sensorycznej dla dzieci i niemowląt</h1>
         </div>
@@ -40,6 +40,8 @@ export default {
     padding-top: 10px;
     padding-right: 10px;
     color: #000;
+    opacity: 0.9;
+    text-align: center;
 }
 #section-a .background-image {
     position: absolute;
@@ -51,12 +53,12 @@ export default {
     padding:0px;
     width: 100%;
     height: 500px;
-    opacity: 0.4;
+    opacity: 0.7;
 }
 
-    @media (min-width: 900px) {
-        #section-a .background-image {
-            background-position-y: -250px;
-        }
-    }
+    /*@media (min-width: 900px) {*/
+        /*#section-a .background-image {*/
+            /*background-position-y: -250px;*/
+        /*}*/
+    /*}*/
 </style>
