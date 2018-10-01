c6a282a6f18439511454f8ca38736742b731a4ed
Piotr <piotrwanio@gmail.com>
2018-10-01 15:41:02 +0200

    Privacy policy

--- assets/components/PrivacyComponent.vue
+++ assets/components/PrivacyComponent.vue
@@ -1,11 +1,105 @@
 <template>
-    <h1>Tu będzie wygenerowany tekst z polityką prywatności.</h1>
-</template>
+                    <main id="content">
+                        <h1 id="head">Polityka prywatności</h1>
+                        <hr>
+                        <h2>Polityka prywatności opisuje zasady przetwarzania przez nas informacji na Twój temat, w tym ciasteczek, czyli tzw. cookies.</h2>
+                        <hr>
+                        <div>
+                            <h2><strong>1. Informacje ogólne</strong></h2>
+                            <ol>
+                                <li>Niniejsza polityka dotyczy Serwisu www, funkcjonującego pod adresem url: <b>rehawalbrzych.vot.pl</b></li>
+                                <li>Operatorem serwisu jest: Kamila Juś Moniuszki 22/2 58-330 Jedlina -Zdrój</li>
 
+                                <li>Adres kontaktowy poczty elektronicznej operatora: rehawalbrzych@gmail.com</li>
+
+                                <li>Serwis realizuje funkcje pozyskiwania informacji o użytkownikach i ich zachowaniu w następujący sposób:
+                                    <ol>
+                                        <li>Poprzez zapisywanie w urządzeniach końcowych plików cookie (tzw. „ciasteczka”).</li>
+                                    </ol>
+                                </li>
+                            </ol>
+                            <h2><strong>2. Wybrane metody ochrony danych stosowane przez Operatora</strong></h2>
+                            <ol><li>Miejsca logowania i wprowadzania danych osobowych są chronione w warstwie transmisji (certyfikat SSL). Dzięki temu dane osobowe i dane logowania, wprowadzone na stronie, zostają zaszyfrowane w komputerze użytkownika i mogą być odczytane jedynie na docelowym serwerze.</li>
+                            </ol>
+                            <h2><strong>3. Hosting</strong></h2>
+                            <ol>
+                                <li>Serwis jest hostowany (technicznie utrzymywany) na serwera operatora: linuxpl.com</li><li>Dane rejestrowe firmy hostingowej: H88 S.A. z siedzibą w Poznaniu, Franklina Roosevelta 22, 60-829 Poznań, wpisana do Krajowego Rejestru Sądowego przez Sąd Rejonowy Poznań – Nowe Miasto i Wilda w Poznaniu, Wydział VIII Gospodarczy Krajowego Rejestru Sądowego pod nr KRS 0000612359, REGON 364261632, NIP 7822622168, kapitał zakładowy 210.000,00 zł w pełni wpłacony.</li><li>Pod adresem <a href="https://linuxpl.com">https://linuxpl.com</a> możesz dowiedzieć się więcej o hostingu i sprawdzić politykę prywatności firmy hostingowej.</li><li>Firma hostingowa:<ul><li>stosuje środki ochrony przed utratą danych (np. macierze dyskowe, regularne kopie bezpieczeństwa),</li><li>stosuje adekwatne środki ochrony miejsc przetwarzania na wypadek pożaru (np. specjalne systemy gaśnicze),</li><li>stosuje adekwatne środki ochrony systemów przetwarzania na wypadek nagłej awarii zasilania (np. podwójne tory zasilania, agregaty, systemy podtrzymania napięcia UPS),</li><li>stosuje środki fizycznej ochrony dostępu do miejsc przetwarzania danych (np. kontrola dostępu, monitoring),</li><li>stosuje środki zapewnienia odpowiednich warunków środowiskowych dla serwerów jako elementów systemu przetwarzania danych (np. kontrola warunków środowiskowych, specjalistyczne systemy klimatyzacji),</li><li>stosuje rozwiązania organizacyjne dla zapewnienia możliwie wysokiego stopnia ochrony i poufności (szkolenia, wewnętrzne regulaminy, polityki haseł itp.),</li><li>powołała Inspektora Ochrony Danych.</li></ul></li>
+
+                                <li>Firma hostingowa w celu zapewnienia niezawodności technicznej prowadzi logi na poziomie serwera. Zapisowi mogą podlegać:
+                                    <ul>
+                                        <li>zasoby określone identyfikatorem URL (adresy żądanych zasobów – stron, plików),</li>
+                                        <li>czas nadejścia zapytania,</li>
+                                        <li>czas wysłania odpowiedzi,</li>
+                                        <li>nazwę stacji klienta – identyfikacja realizowana przez protokół HTTP,</li>
+                                        <li>informacje o błędach jakie nastąpiły przy realizacji transakcji HTTP,</li>
+                                        <li>adres URL strony poprzednio odwiedzanej przez użytkownika (referer link) – w przypadku gdy przejście do Serwisu nastąpiło przez odnośnik,</li>
+                                        <li>informacje o przeglądarce użytkownika,</li>
+                                        <li>informacje o adresie IP,</li>
+                                        <li>informacje diagnostyczne związane z procesem samodzielnego zamawiania usług poprzez rejestratory na stronie,</li>
+                                        <li>informacje związane z obsługą poczty elektronicznej kierowanej do Operatora oraz wysyłanej przez Operatora.</li>
+                                    </ul>
+                                </li>
+                            </ol>
+                            <h2><strong>4. Logi Administratora</strong></h2>
+                            <ol>
+                                <li>Informacje o zachowaniu użytkowników w serwisie mogą podlegać logowaniu. Dane te są wykorzystywane w celu administrowania serwisem.</li>
+                            </ol>
+                            <h2><strong>5. Informacja o plikach cookies</strong></h2>
+                            <ol>
+                                <li>Serwis korzysta z plików cookies.</li>
+                                <li>Pliki cookies (tzw. „ciasteczka”) stanowią dane informatyczne, w szczególności pliki tekstowe, które przechowywane są w urządzeniu końcowym Użytkownika Serwisu i przeznaczone są do korzystania ze stron internetowych Serwisu. Cookies zazwyczaj zawierają nazwę strony internetowej, z której pochodzą, czas przechowywania ich na urządzeniu końcowym oraz unikalny numer.</li>
+                                <li>Podmiotem zamieszczającym na urządzeniu końcowym Użytkownika Serwisu pliki cookies oraz uzyskującym do nich dostęp jest operator Serwisu.</li>
+                                <li>Pliki cookies wykorzystywane są w następujących celach:
+                                    <ol>
+                                        <li>utrzymanie sesji użytkownika Serwisu (po zalogowaniu), dzięki której użytkownik nie musi na każdej podstronie Serwisu ponownie wpisywać loginu i hasła;</li>
+                                        <li>realizacji celów określonych powyżej w części "Istotne techniki marketingowe";</li>
+                                    </ol>
+                                </li>
+                                <li>W ramach Serwisu stosowane są dwa zasadnicze rodzaje plików cookies: „sesyjne” (session cookies) oraz „stałe” (persistent cookies). Cookies „sesyjne” są plikami tymczasowymi, które przechowywane są w urządzeniu końcowym Użytkownika do czasu wylogowania, opuszczenia strony internetowej lub wyłączenia oprogramowania (przeglądarki internetowej). „Stałe” pliki cookies przechowywane są w urządzeniu końcowym Użytkownika przez czas określony w parametrach plików cookies lub do czasu ich usunięcia przez Użytkownika.</li>
+                                <li>Oprogramowanie do przeglądania stron internetowych (przeglądarka internetowa) zazwyczaj domyślnie dopuszcza przechowywanie plików cookies w urządzeniu końcowym Użytkownika. Użytkownicy Serwisu mogą dokonać zmiany ustawień w tym zakresie.&nbsp;Przeglądarka internetowa umożliwia usunięcie plików cookies. Możliwe jest także automatyczne blokowanie plików cookies Szczegółowe informacje na ten temat zawiera pomoc lub dokumentacja przeglądarki internetowej.</li>
+                                <li>Ograniczenia stosowania plików cookies mogą wpłynąć na niektóre funkcjonalności dostępne na stronach internetowych Serwisu.</li>
+                                <li>Pliki cookies zamieszczane w urządzeniu końcowym Użytkownika Serwisu wykorzystywane mogą być również przez współpracujące z operatorem Serwisu podmioty, w szczególności dotyczy to firm: Google (Google Inc. z siedzibą w USA), Facebook (Facebook Inc. z siedzibą w USA), Twitter (Twitter Inc. z siedzibą w USA).</li>
+                            </ol>
+                            <h2><strong>6. Zarządzanie plikami cookies – jak w praktyce wyrażać i cofać zgodę?</strong></h2>
+                            <ol>
+                                <li>Jeśli użytkownik nie chce otrzymywać plików cookies, może zmienić ustawienia przeglądarki. Zastrzegamy, że wyłączenie obsługi plików cookies niezbędnych dla procesów uwierzytelniania, bezpieczeństwa, utrzymania preferencji użytkownika może utrudnić,&nbsp;a w skrajnych przypadkach może uniemożliwić korzystanie ze stron www</li>
+                                <li>W celu zarządzania ustawienia cookies wybierz z listy poniżej przeglądarkę internetową, której używasz i postępuj zgodnie z instrukcjami:
+                                    <ul>
+                                        <li><a href="https://support.microsoft.com/pl-pl/help/10607/microsoft-edge-view-delete-browser-history">Edge</a></li>
+                                        <li><a href="https://support.microsoft.com/pl-pl/help/278835/how-to-delete-cookie-files-in-internet-explorer">Internet Explorer</a></li>
+                                        <li><a href="http://support.google.com/chrome/bin/answer.py?hl=pl&amp;answer=95647">Chrome</a></li>
+                                        <li><a href="http://support.apple.com/kb/PH5042">Safari</a></li>
+                                        <li><a href="http://support.mozilla.org/pl/kb/W%C5%82%C4%85czanie%20i%20wy%C5%82%C4%85czanie%20obs%C5%82ugi%20ciasteczek">Firefox</a></li>
+                                        <li><a href="http://help.opera.com/Windows/12.10/pl/cookies.html">Opera</a></li>
+                                    </ul>
+                                    <p>Urządzenia mobilne:</p>
+                                    <ul>
+                                        <li><a href="http://support.google.com/chrome/bin/answer.py?hl=pl&amp;answer=95647">Android</a></li>
+                                        <li><a href="http://support.apple.com/kb/HT1677?viewlocale=pl_PL">Safari (iOS)</a></li>
+                                        <li><a href="http://www.windowsphone.com/pl-pl/how-to/wp7/web/changing-privacy-and-other-browser-settings">Windows Phone</a></li>
+                                    </ul>
+                                </li>
+                            </ol>
+
+                        </div>
+                    </main>
+</template>
 <script>
 
 </script>
 
-<style>
+<style scoped>
+    h1, h2, ul, ol{
+        text-align: left;
+    }
 
+    #content {
+        width: 90%;
+        margin: auto;
+    }
+
+    #head {
+        text-align: center;
+        margin-top: 20px;
+    }
 </style>
